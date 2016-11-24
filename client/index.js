f2fe75e1d681db46c7504bbe4ca270605e72d8f6
Muhammad Asif Javed <axif.javed@gmail.com>
2016-11-24 23:11:02 +0500

    static assets folder created

--- client/index.js
+++ client/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('app'));
